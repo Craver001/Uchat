c8fb561c11c38d5ea4046eaca84b6ca9c903acdc
Rene Alfred Tresmonte <146701461+ReneMadGaming@users.noreply.github.com>
2024-04-18 16:40:22 +0800

    bottom navigation icon
    updated the code added bottom navigation icon

--- Navigator/NavigatorElement.js
+++ Navigator/NavigatorElement.js
@@ -3,7 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MainScreen from '../Screen/MainScreen';
 import ChatActivityPage from '../Screen/chatActivityPage';
-import settings from '../Screen/Settings';
+
+
+import { Ionicons } from '@expo/vector-icons';
+import ChatSettings from '../Screen/Settings';
 
 
 const Stack = createStackNavigator();
@@ -12,8 +15,20 @@ const Tab = createBottomTabNavigator();
 function ChatUI() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={MainScreen} />
-      <Tab.Screen name="Settings" component={settings} />
+      <Tab.Screen name="Home" component={MainScreen}options={{
+         headerShown: false,
+        tabBarLabel:'chat',
+        tabBarIcon:({color, size})=>(
+          <Ionicons name="chatbox-outline" size={size} color={color} />
+        )
+        }}/>
+      <Tab.Screen name="Settings" component={ChatSettings}options ={{
+        headerShown: false,
+        tabBarLabel:'Settings',
+        tabBarIcon:({color, size})=>(
+          <Ionicons name="settings-outline" size={size} color={color}/>
+        )
+        }} />
     </Tab.Navigator>
   );
 }
